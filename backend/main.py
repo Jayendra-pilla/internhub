e1a716dd54bb2b091b5aa244ae8b5932768c6fb5
Jayendra <jayendrapilla@gmail.com>
2026-06-20 12:22:29 +0530

    Fix CORS for Vercel

--- backend/main.py
+++ backend/main.py
@@ -81,11 +81,12 @@ async def require_admin(user=Depends(get_current_user)):
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:5174",
-        "http://127.0.0.1:5174"
-    ],
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://localhost:5174",
+    "http://127.0.0.1:5174",
+    "https://internhub-mocha.vercel.app",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
